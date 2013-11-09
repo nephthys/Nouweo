a600e85277ef942c7d7b65893360b25388bb70d7
Maxime Lorant <maxime.lorant@gmail.com>
2013-11-09 18:54:02 +0100

    Make the settings file works almost 'out of the box'

--- nouweo/settings.example.py
+++ nouweo/settings.example.py
@@ -1,4 +1,7 @@
-# Django settings for nouweo project.
+# Django settings for nouweo project
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -73,6 +76,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    BASE_DIR + '/static',
 )
 
 # List of finder classes that know how to find static files in
@@ -112,6 +116,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    BASE_DIR + '/templates',
 )
 
 INSTALLED_APPS = (
