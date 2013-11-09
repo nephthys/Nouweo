5d2670f8caf8aa1be1a4484217b7920cce881a21
Camille Bouiller <camille@nouweo.com>
2013-11-10 00:25:37 +0100

    Move urls in apps

--- nouweo/urls.py
+++ nouweo/urls.py
@@ -1,39 +1,10 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, include
 
 from django.contrib import admin
 admin.autodiscover()
 
-from posts.views import *
-from community.views import *
-
 urlpatterns = patterns('',
-    url(r'^$', homepage, name='home'),
-
-    url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^comments/posted/$', comment_posted),
-    url(r'^comments/', include('django.contrib.comments.urls')),
-
-    url(r'^add/news/$', add_news, name='add_news'),
-    url(r'^edit/news/(?P<id>\d+)/$', edit_news, name='edit_news'),
-    url(r'^posts/draft/$', view_posts_draft, name='posts_draft'),
-    url(r'^posts/pending/$', view_posts_pending, name='posts_pending'),
-    url(r'^posts/propose/(?P<post_id>\d+)/$', post_propose,
-        name='post_propose'),
-
-    url(r'^users/(?P<id>\d+)/$', view_user_profile, name='user_profile'),
-
-    url(r'^rating/(?P<model>post|idea|comment)/(?P<id>\d+)/(?P<direction>up|down)/$', add_vote,
-        name='add_vote'),
-
-    url(r'^(?P<cat>[\-\d\w]+)/(?P<slug>[\-\d\w]+)/$', view_post,
-        name='post_view'),
-    url(r'^(?P<cat>[\-\d\w]+)/(?P<slug>[\-\d\w]+)/revision/(?P<revision>\d+)/$', view_post,
-        name='post_revision'),
-    url(r'^(?P<cat>[\-\d\w]+)/(?P<slug>[\-\d\w]+)/revision/(?P<revision>\d+)/select/$', select_revision,
-        name='post_revision_select'),
-    url(r'^(?P<cat>[\-\d\w]+)/(?P<slug>[\-\d\w]+)/revision/(?P<revision>\d+)/delete/$', delete_revision,
-        name='post_revision_delete'),
-    url(r'^(?P<cat>[\-\d\w]+)/(?P<slug>[\-\d\w]+)/revisions/$', view_revisions,
-        name='post_revisions'),
+    ('^admin/', include(admin.site.urls)),
+    ('^', include('community.urls')),
+    ('^', include('posts.urls')),
 )
---
+++ posts/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('posts.views',
+	url(r'^$', 'homepage', name='home'),
+	url(r'^add/news/$', 'add_news', name='add_news'),
+    url(r'^edit/news/(?P<id>\d+)/$', 'edit_news', name='edit_news'),
+    url(r'^posts/draft/$', 'view_posts_draft', name='posts_draft'),
+    url(r'^posts/pending/$', 'view_posts_pending', name='posts_pending'),
+    url(r'^posts/propose/(?P<post_id>\d+)/$', 'post_propose',
+        name='post_propose'),
+
+    url(r'^(?P<cat>[\-\d\w]+)/(?P<slug>[\-\d\w]+)/$', 'view_post',
+        name='post_view'),
+    url(r'^(?P<cat>[\-\d\w]+)/(?P<slug>[\-\d\w]+)/revision/(?P<revision>\d+)/$', 'view_post',
+        name='post_revision'),
+    url(r'^(?P<cat>[\-\d\w]+)/(?P<slug>[\-\d\w]+)/revision/(?P<revision>\d+)/select/$', 'select_revision',
+        name='post_revision_select'),
+    url(r'^(?P<cat>[\-\d\w]+)/(?P<slug>[\-\d\w]+)/revision/(?P<revision>\d+)/delete/$', 'delete_revision',
+        name='post_revision_delete'),
+    url(r'^(?P<cat>[\-\d\w]+)/(?P<slug>[\-\d\w]+)/revisions/$', 'view_revisions',
+        name='post_revisions'),
+)
