ed401df999bc1b7ce758018aee90fcdc1fafe696
Camille Bouiller <camille@nouweo.com>
2013-11-09 18:38:22 +0100

    Add dependancies missing: markdown and django.contrib.comments in settings

--- nouweo/settings.example.py
+++ nouweo/settings.example.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.comments',
     'django.contrib.admin',
     'posts',
     'community',
